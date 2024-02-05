a234f4572b7bad552282c4d5be95e481c5be119f
LaThanhTrong <lathanhtrong32200@gmail.com>
2024-02-05 13:48:34 +0700

    setup env

--- CODE/RESTAURANT_MANAGEMENT/Database.cs
+++ CODE/RESTAURANT_MANAGEMENT/Database.cs
@@ -1,46 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
-namespace RESTAURANT_MANAGEMENT
-{
-    internal class Database
+class Database
     {
-        public static SqlConnection Connection;
-        public static bool Connect()
+    public static SqlConnection Connection;
+    public static bool Connect()
+    {
+        try
         {
-            try
-            {
-                Connection = new SqlConnection("Data Source = KAITO; Initial Catalog = RESTAURANT_MANAGEMENT; Integrated Security = True;");
-                Connection.Open();
-                Console.WriteLine("Successfully connect to database server");
-                return true;
-            }catch (Exception ex)
-            {
-                MessageBox.Show("Failed to connect database server");
-                Console.WriteLine("ERROR DATABASE: " + ex.ToString());
-                return false;
+            DotNetEnv.Env.TraversePath().Load();
+            var host = Environment.GetEnvironmentVariable("HOST");
+            var database = Environment.GetEnvironmentVariable("DATABASE");
+            var user = Environment.GetEnvironmentVariable("USER");
+            var password = Environment.GetEnvironmentVariable("PASSWORD");
+            Connection = new SqlConnection($"Server={host}; Database={database}; uid={user}; pwd={password}");
+            Connection.Open();
+            Console.WriteLine("Successfully connect to database server");
+            return true;
+        }catch (Exception ex)
+        {
+            MessageBox.Show("Failed to connect database server");
+            Console.WriteLine("ERROR DATABASE: " + ex.ToString());
+            return false;
 
-            }
         }
+    }
 
-        public static bool Close()
+    public static bool Close()
+    {
+        try
         {
-            try
-            {
-                Connection.Close();
-                return true;
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-                return false;
-            }
+            Connection.Close();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            return false;
         }
     }
 }
